b239c81d75c183c8067a0b5efc5fc5e481b17b66
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-02-27 20:07:02 -0600

    Update complex-numbers.js

--- complex-numbers.js
+++ complex-numbers.js
@@ -1,3 +1,47 @@
 class Complex {
-
+	constructor(real,im) {
+		this.real = real;
+		this.im = im;
+	}
+	get conjugate() {
+		return new Complex(this.real,-this.im);
+	}
+	get neg() {
+		return new Complex(-this.real,-this.im);
+	}
+	get recip() {
+		return new Complex(this.real/(this.real**2+this.im**2),-this.im/(this.real**2+this.im**2));
+	}
+	static add(a,b) {
+		return new Complex(a.real+b.real,a.im+b.im);
+	}
+	static mult(a,b) {
+		return new Complex(a.real*b.real-a.im*b.im,a.im*b.real+a.real*b.im);
+	}
+	static sub(a,b) {
+		return Complex.add(a,b.neg);
+	}
+	static div(a,b) {
+		return Complex.mult(a,b.recip);
+	}
+	static exp(a) {
+		return new Complex(Math.cos(a.im)*Math.E**a.real,Math.sin(a.im)*Math.E**a.real);
+	}
+	static ln(a) {
+		return new Complex(Math.ln(Math.sqrt(a.real**2+a.im**2)),Math.atan2(a.im,a.real));
+	}
+	static power(a,b) {
+		return Complex.exp(Complex.mult(b,Complex.ln(a)));
+	}
+	static sin(a) {
+		return Complex.div(Complex.exp(Complex.sub(Complex.mult(a,Complex.i),Complex.mult(a,Complex.i.neg))),new Complex(0,2));
+	}
+	static cos(a) {
+		return Complex.div(Complex.exp(Complex.add(Complex.mult(a,Complex.i),Complex.mult(a,Complex.i.neg))),new Complex(2,0));
+	}
+	static tan(a) {
+		return Complex.div(Complex.sin(a),Complex.cos(a));
+	}
 }
+Complex.i = new Complex(0,1);
+Complex.one = new Complex(1,0);
