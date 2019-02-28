20918ea36a30401ebddd6677aeef8a637a6fb179
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-02-27 21:35:30 -0600

    Update mandelbrots.js

--- mandelbrots.js
+++ mandelbrots.js
@@ -27,7 +27,7 @@ function render() {
 	
 	for(let x=0; x < width; x++) {
 		for(let y=0; y < height; y++) {
-			let n = iterate(x/magnificationFactor-posX,y/magnificationFactor-posY,100);
+			let n = iterate(x/magnificationFactor+width/2+posX,y/magnificationFactor-posY+height/2+posY,100);
 			if(n = 100) {
 				ctx.fillStyle = "rgb(0,0,0)";
 			} else {
